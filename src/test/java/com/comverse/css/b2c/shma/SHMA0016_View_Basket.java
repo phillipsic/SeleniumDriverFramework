28acc5ef3e70b50f7d55c6d97b9f200b21b4e135
Ian Phillips <ian.phillips@comverse.com>
2014-06-13 17:50:01 +0200

    Updating test.  Still not passing - setSpendingLimit method needs to be updated to take name of spending limit.

--- src/test/java/com/comverse/css/b2c/shma/SHMA0016_View_Basket.java
+++ src/test/java/com/comverse/css/b2c/shma/SHMA0016_View_Basket.java
@@ -13,8 +13,10 @@ import com.comverse.css.data.ACC.ACC_CSSPQABatteryPhoneACC1;
 import com.comverse.css.data.PO.PO_ResidentialUltraPostpaid;
 import com.comverse.css.data.SO.SO_DIYeCountdownExtra;
 import com.comverse.data.apps.B2C;
+import com.comverse.css.data.DEV.VD_CSSPQAVoiceDeviceStandardPhonesDEV1;
 
 public class SHMA0016_View_Basket extends CSSTest {
+
     private StringBuffer verificationErrors = new StringBuffer();
 
     @Override
@@ -31,6 +33,8 @@ public class SHMA0016_View_Basket extends CSSTest {
             PO_ResidentialUltraPostpaid po_ResidentialUltraPostpaid = new PO_ResidentialUltraPostpaid();
             ACC_CSSPQABatteryPhoneACC1 accBatteryPhone1 = new ACC_CSSPQABatteryPhoneACC1();
             SO_DIYeCountdownExtra so_DIYeCountDownExtra = new SO_DIYeCountdownExtra();
+            VD_CSSPQAVoiceDeviceStandardPhonesDEV1 vd_StandardPhoneDEV1 = new VD_CSSPQAVoiceDeviceStandardPhonesDEV1();
+
             launchCSSApplication();
             String uniqueTimeStamp = Common.generateTimeStamp();
             double poNRC = Double.parseDouble(po_ResidentialUltraPostpaid.getNRC());
@@ -43,12 +47,13 @@ public class SHMA0016_View_Basket extends CSSTest {
             // ChooseYourPrimaryOffer chooseYourPrimaryOffer =
             // chooseYourPrimaryOfferInThisAccountBundle.clickSwitchToBrowsePrimaryOffers();
             ChooseYourDevice chooseYourDevice = chooseYourPrimaryOffer.selectPrimaryOfferByNameWithDevice(po_ResidentialUltraPostpaid.getOfferName());
-            EnterYourSubscriptionDetails enterYourSubscriptionDetails = chooseYourDevice.clickNoThanks();
+
+            ChooseYourHandset chooseYourHandset = chooseYourDevice.clickSelectVoiceDevices();
+            EnterYourSubscriptionDetails enterYourSubscriptionDetails = chooseYourHandset.selectHandsetExpectingSubscriptionDetails(vd_StandardPhoneDEV1.getDeviceName());
 
             enterYourSubscriptionDetails.enterDefaultIdentityAddressPhoneEmail(uniqueTimeStamp);
             SelectOffersForYourSubscriber selectOffersForYourSubscriber = enterYourSubscriptionDetails.clickOk();
-            selectOffersForYourSubscriber.selectOffersForSubscriber(so_DIYeCountDownExtra.getOfferName());
-
+  
             ConfigureOffers configureOffers = selectOffersForYourSubscriber.clickContinue();
             ConfigureBalance configureBalance = configureOffers.clickContinueExpectingConfigureBalance();
             configureBalance.setSpendingLimit("120");
@@ -59,12 +64,12 @@ public class SHMA0016_View_Basket extends CSSTest {
             chooseAccessories.clickContinue();
 
             // Verification of added PO & Accessory with total price .
-
             Common.assertTextOnPage(tool, po_ResidentialUltraPostpaid.getOfferName());
             Common.assertTextOnPage(tool, String.valueOf(poNRC));
             Common.assertTextOnPage(tool, accBatteryPhone1.getAccessoryName());
             Common.assertTextOnPage(tool, String.valueOf(accessoryTotalPrice));
             Common.assertTextOnPage(tool, String.valueOf(poNRC + accessoryTotalPrice));
+            Common.assertTextOnPage(tool, vd_StandardPhoneDEV1.getDeviceName());
 
             // bugId = "NoBug";
             test.setResult("pass");
