05ba704a65cf1ca45ed8b41a2a1f41b504aac472
Ian Phillips <ian.phillips@comverse.com>
2014-06-16 18:07:15 +0200

    Corrected Role of OCMPub user.

--- src/main/java/com/comverse/data/users/OCMPub.java
+++ src/main/java/com/comverse/data/users/OCMPub.java
@@ -5,8 +5,9 @@ import com.comverse.common.User;
 public class OCMPub extends User {
 
     public OCMPub() throws Exception {
-        setRole("OCMPub");
+       
         setLogin();
         setPassword();
+        setRole("OCM Publisher");
     }
 }
