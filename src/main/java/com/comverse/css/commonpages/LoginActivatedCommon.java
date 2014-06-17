74ee92b6caaa1f2dc42b38ef9d393929c82bda49
Ian Phillips <ian.phillips@comverse.com>
2014-06-17 09:47:28 +0200

    Added new pages

---
+++ src/main/java/com/comverse/css/commonpages/LoginActivatedCommon.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.comverse.css.commonpages;
+
+
+import com.comverse.common.AutomationTool;
+import com.comverse.common.Test;
+import com.comverse.common.User;
+
+public class LoginActivatedCommon extends CommonMenu {
+
+    public LoginActivatedCommon(AutomationTool tool, Test test, User user) throws Exception {
+        super(tool, test, user);
+        String currentScreen = tool.getTitle(tool);
+        String expectedScreen = "Login activated";
+
+        if (!expectedScreen.equals(currentScreen)) {
+            throw new IllegalStateException("Expecting: " + expectedScreen + " , but got: " + currentScreen);
+        }
+    }
+    
+     public ModifyLoginPasswordCommon clickChangePassword() throws Exception {
+
+        tool.clickUsingXPath(tool, "//input[@value='Change Password']");
+        return new ModifyLoginPasswordCommon(tool, test, user);
+    }
+
+  
+}
