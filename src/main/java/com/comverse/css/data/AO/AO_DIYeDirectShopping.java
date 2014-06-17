fbb0a15dc2de152c5eef997111f3c769be2b4af2
Ian Phillips <ian.phillips@comverse.com>
2014-06-17 17:03:37 +0200

    Added account offer - AO_DIYeDirectShopping

---
+++ src/main/java/com/comverse/css/data/AO/AO_DIYeDirectShopping.java
@@ -0,0 +1,9 @@
+package com.comverse.css.data.AO;
+
+import com.comverse.css.data.Offer;
+
+public class AO_DIYeDirectShopping extends Offer {
+    public AO_DIYeDirectShopping() {
+        setOfferName("DIYe - Direct Shopping");
+    }
+}
