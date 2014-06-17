aa4b2459067285f7c0156f4a57b27653a975a442
Ian Phillips <ian.phillips@comverse.com>
2014-06-17 18:30:38 +0200

    Cleaning up

--- src/main/java/com/comverse/css/b2c/ModifyCustomerDetails.java
+++ src/main/java/com/comverse/css/b2c/ModifyCustomerDetails.java
@@ -27,42 +27,36 @@ public class ModifyCustomerDetails extends B2CMenu {
 
     public void enterAddressLineOne(String addressLine1) throws Exception {
 
-        
         tool.enterStringUsingId(tool, "address_line_1", addressLine1);
 
     }
 
     public void enterAddressLineTwo(String addressLine2) throws Exception {
 
-        
         tool.enterStringUsingId(tool, "address_line_2", addressLine2);
 
     }
 
     public void enterAddressLineThree(String addressLine3) throws Exception {
 
-        
         tool.enterStringUsingId(tool, "address_line_3", addressLine3);
 
     }
 
     public void enterAddressLineFour(String addressLine4) throws Exception {
 
-        
         tool.enterStringUsingId(tool, "address_line_4", addressLine4);
 
     }
 
     public void enterZipCode(String zipCode) throws Exception {
 
-        
         tool.enterStringUsingId(tool, "zip", zipCode);
 
     }
 
     public void enterCity(String city) throws Exception {
 
-        
         tool.enterStringUsingId(tool, "city", city);
 
     }
@@ -75,28 +69,24 @@ public class ModifyCustomerDetails extends B2CMenu {
 
     public void enterFirstName(String firstName) throws Exception {
 
-        
         tool.enterStringUsingId(tool, "first_name", firstName);
 
     }
 
     public void enterLastName(String lastName) throws Exception {
 
-        
         tool.enterStringUsingId(tool, "last_name", lastName);
 
     }
 
     public void enterEmailAddress(String emailAddress) throws Exception {
 
-        
         tool.enterStringUsingId(tool, "company_email", emailAddress);
 
     }
 
     public void enterLogin(String uniqueTimeStamp) throws Exception {
 
-        
         tool.enterStringUsingId(tool, "login", uniqueTimeStamp);
         System.out.println("Login - " + uniqueTimeStamp);
 
@@ -104,7 +94,6 @@ public class ModifyCustomerDetails extends B2CMenu {
 
     public void enterPassword(String password) throws Exception {
 
-        
         tool.enterStringUsingId(tool, "password", password);
         System.out.println("Password - " + password);
 
@@ -112,7 +101,6 @@ public class ModifyCustomerDetails extends B2CMenu {
 
     public void enterConfirmPassword(String confirmPassword) throws Exception {
 
-        
         tool.enterStringUsingId(tool, "confirm_password", confirmPassword);
 
     }
@@ -125,19 +113,16 @@ public class ModifyCustomerDetails extends B2CMenu {
 
     public void enterSecretAnswer(String secretAnswer) throws Exception {
 
-        
         tool.enterStringUsingId(tool, "secret_answer", secretAnswer);
     }
 
     public void enterNationalId(String national_id) throws Exception {
 
-        
         tool.enterStringUsingId(tool, "national_id", national_id);
     }
 
     public void enterDateOfBirth(String dob) throws Exception {
 
-        
         tool.enterStringUsingId(tool, "date_of_birth", dob);
     }
 
