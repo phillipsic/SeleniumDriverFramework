314ffb5fa1b4db4dc999b5c6cce6218aaaab9edf
Ian Phillips <ian.phillips@comverse.com>
2014-06-18 09:44:34 +0200

    Added prepaid business offer class

---
+++ src/main/java/com/comverse/css/data/PO/PO_BusinessHQEmployee.java
@@ -0,0 +1,13 @@
+package com.comverse.css.data.PO;
+
+import com.comverse.css.data.Offer;
+
+public class PO_BusinessHQEmployee extends Offer {
+
+    public PO_BusinessHQEmployee() {
+        setOfferName("Business - HQ - Employee");
+        setOfferCustomerType("Business");
+        setRC("150.00");
+        setNRC("0.00");
+    }
+}
