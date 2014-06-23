99598fd770f6e66cc4d06a2d7278a2a16e303139
Ian Phillips <ian.phillips@comverse.com>
2014-06-23 14:49:53 +0200

    Added better debug info.

--- src/main/java/com/comverse/css/commonpages/AddNewCaseCommon.java
+++ src/main/java/com/comverse/css/commonpages/AddNewCaseCommon.java
@@ -9,15 +9,17 @@ import com.comverse.common.Test;
 import com.comverse.common.User;
 
 public class AddNewCaseCommon extends CommonMenu {
-    static String expectedScreen = "Add Member - Confirmation";
 
     public AddNewCaseCommon(AutomationTool tool, Test test, User user) throws Exception {
         super(tool, test, user);
 
-        // Check that we're on the right page.
-        if (!"Add case".equals(tool.getTitle())) {
-            throw new IllegalStateException("This is not the Add case page");
-        }
+        String currentScreen = tool.getTitle();
+        String expectedScreen = "Add case";
+
+        if (!expectedScreen.equals(currentScreen)) {
+            throw new IllegalStateException("Expecting: " + expectedScreen + " , but got: " + currentScreen);
+        }  // Check that we're on the right page.
+
     }
 
     public void selectCaseType(String caseType) throws Exception {
