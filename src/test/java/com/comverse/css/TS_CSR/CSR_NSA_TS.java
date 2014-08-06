aadc3c4982c8815d216d170f02b40d85f9db109d
Ian Phillips <ian.phillips@comverse.com>
2014-08-06 12:58:04 +0200

    Reformatted to make file more readable

--- src/test/java/com/comverse/css/TS_CSR/CSR_NSA_TS.java
+++ src/test/java/com/comverse/css/TS_CSR/CSR_NSA_TS.java
@@ -10,7 +10,9 @@ import com.comverse.css.csr.nsa.NSA0025_Update_administrative_data;
 import com.comverse.css.csr.nsa.NSA0030_Capture_billing_information;
 
 @RunWith(Suite.class)
-@SuiteClasses({ NSA0010_New_subscriber_for_an_existing_customer.class, NSA0020_Review_existing_account.class,
-               NSA0025_Update_administrative_data.class, NSA0030_Capture_billing_information.class })
+@SuiteClasses({NSA0010_New_subscriber_for_an_existing_customer.class,
+    NSA0020_Review_existing_account.class,
+    NSA0025_Update_administrative_data.class,
+    NSA0030_Capture_billing_information.class})
 public class CSR_NSA_TS {
 }
