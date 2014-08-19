205175524642569f1cb218d0284f863d1d00fd5d
Ian Phillips <ian.phillips@comverse.com>
2014-08-19 18:29:21 +0200

    Updated verification text

--- src/test/java/com/comverse/css/csr/aml/AML0010_Disconnect_Account.java
+++ src/test/java/com/comverse/css/csr/aml/AML0010_Disconnect_Account.java
@@ -47,7 +47,7 @@ public class AML0010_Disconnect_Account extends CSSTest {
             disconnectAccount.clickOk();
             Common.isTextOnPage(tool, "Account Details");
 
-            accountDashboard.waitUntilSubscriberStatusChanged("Disconnect Requested");
+            accountDashboard.waitUntilSubscriberStatusChanged("Status:  Disconnected");
 
             test.setResult("pass");
         } catch (AlreadyRunException e) {
