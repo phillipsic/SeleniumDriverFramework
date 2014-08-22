6fb056a37ae64b1bf5e9ed09ebed27cd3ac576f3
Ian Phillips <ian.phillips@comverse.com>
2014-08-22 17:51:33 +0200

    Improved test.

--- src/test/java/com/comverse/css/csr/pem/PEM0430_Merge_persons.java
+++ src/test/java/com/comverse/css/csr/pem/PEM0430_Merge_persons.java
@@ -8,14 +8,10 @@ import com.comverse.css.common.AlreadyRunException;
 import com.comverse.css.common.CSSTest;
 import com.comverse.css.common.Common;
 import com.comverse.css.common.Prep;
-import com.comverse.css.commonpages.AccountDetailsCommon;
-import com.comverse.css.commonpages.PersonManagementCommon;
-import com.comverse.css.commonpages.PersonMergeCommon;
-import com.comverse.css.csr.AccountDetails;
-import com.comverse.css.csr.IdentifyCustomer;
-import com.comverse.css.csr.WorkSpace;
+import com.comverse.css.csr.*;
 import com.comverse.data.apps.CSR;
 import com.comverse.data.users.CSRAdmin;
+import static org.junit.Assert.assertEquals;
 
 public class PEM0430_Merge_persons extends CSSTest {
 
@@ -41,21 +37,22 @@ public class PEM0430_Merge_persons extends CSSTest {
             IdentifyCustomer manageAccount = workSpace.gotoManageAccount();
             AccountDetails accountDetails = manageAccount.gotoAccountDashboardUsingAccountLastName(accountLastName);
 
-            PersonManagementCommon personManagement = accountDetails.clickPersonManagement();
+            PersonManagement personManagement = accountDetails.clickPersonManagement();
 
             personManagement.addCMP();
             personManagement.clickActions(Common.getPersonFirstName());
-            PersonMergeCommon personMerge = personManagement.clickPersonMerge();
-            personMerge.setLastName(personManagement.person3.getPersonLastNameProperty());
+            PersonMerge personMerge = personManagement.clickPersonMerge();
+            String personLastName = personManagement.person3.getPersonLastNameProperty();
+            personMerge.setLastName(personLastName);
             personMerge.clickPersonSearch();
             personMerge.clickSelect();
 
-            personMerge.clickOKFromMerge();
-            // TODO - need to check the correct person has been set to inactive
-            // - there could be other inactive users on the page.
-            Common.assertTextOnPage(tool, "inactive");
+            personManagement = personMerge.clickOKFromMerge();
 
-            // test.setResult("pass");
+            assertEquals("inactive", personManagement.getPersonStatus(personLastName));
+
+            test.setResult("pass");
+
         } catch (AlreadyRunException e) {
         } catch (Exception e) {
             verificationErrors.append(e.getMessage());
