2985520f1bdc49a88d4bf0c181192c45a7d14a28
Ian Phillips <phillipsic@gmail.com>
2015-04-13 16:06:15 +0200

    Created new app class MercuryTours.

---
+++ src/main/java/com/company/mercurytours/MercuryTours.java
@@ -0,0 +1,12 @@
+package com.company.mercurytours;
+
+import com.framework.common.Application;
+
+public class MercuryTours extends Application {
+
+    public MercuryTours() throws Exception {
+        setName("Mercury Tours");
+        setCommonName("Mercury Tours");
+       
+    }
+}
