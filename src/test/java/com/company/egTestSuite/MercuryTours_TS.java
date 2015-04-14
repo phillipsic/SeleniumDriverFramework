cb9447a2f9375d144144df28212a34f6254cf190
Ian Phillips <phillipsic@gmail.com>
2015-04-14 15:45:48 +0200

    Renamed test suite file

---
+++ src/test/java/com/company/egTestSuite/MercuryTours_TS.java
@@ -0,0 +1,11 @@
+package com.company.egTestSuite;
+
+import com.company.mercurytours.CHK001_RegisterUser;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CHK001_RegisterUser.class})
+public class MercuryTours_TS {
+}
