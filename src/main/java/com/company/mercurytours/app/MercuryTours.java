e9993c5e109621ccf6534afa988b1fe12925285a
Ian Phillips <phillipsic@gmail.com>
2015-04-14 15:52:17 +0200

    Added comments to file

--- src/main/java/com/company/mercurytours/app/MercuryTours.java
+++ src/main/java/com/company/mercurytours/app/MercuryTours.java
@@ -8,5 +8,12 @@ public class MercuryTours extends Application {
         setName("MercuryTours"); // Also used as a key in INI file for application name.
         setCommonName("Mercury Tours");
        
+        
+        /*
+        The base URL should not be set here but in the <ENV>_init.properties file.
+        The base URL could change from one environment to another.
+        If there is no application name in the URL then leave out of the 
+        <ENV>_init.properties file.
+        */
     }
 }
