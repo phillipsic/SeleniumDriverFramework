bc80b6ad2bef6e4e2373a78073a5d227fe39cc6f
Ian Phillips <phillipsic@gmail.com>
2015-04-14 15:49:12 +0200

    Added comments to file

--- src/main/java/com/company/mercurytours/users/JoeBloggs.java
+++ src/main/java/com/company/mercurytours/users/JoeBloggs.java
@@ -5,12 +5,18 @@ import com.framework.common.User;
 public class JoeBloggs extends User {
 
     public JoeBloggs() throws Exception {
-        setRole("NormalUser");
-        
-        /* Following will be read from INI file       
-        */
+
+        /* The role is needed to find the users login and password
+         in the <ENV>_init.properties file
+         LOGIN.NormalUser=JoeBlogs
+         PASSWD.NormalUser=J03Bl0g5        
+         */
+        setRole("NormalUser"); //the role is needed to find the 
+
+        /* Following will be read from INI file using the set role above      
+         */
         setLogin();
         setPassword();
-       // setRealm();
+        // setRealm();
     }
 }
