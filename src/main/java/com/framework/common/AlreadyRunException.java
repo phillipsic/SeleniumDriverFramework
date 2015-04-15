947eb2d68429c10385bd329854557d6505d670d3
Ian Phillips <phillipsic@gmail.com>
2015-04-15 15:36:30 +0200

    Added comment to explain the reason for the class.

--- src/main/java/com/framework/common/AlreadyRunException.java
+++ src/main/java/com/framework/common/AlreadyRunException.java
@@ -7,7 +7,10 @@ package com.framework.common;
 public class AlreadyRunException extends Exception {
 
     /**
-     * 
+     * This class is used to PASS a test without running it.
+     * When the DB is installed each test will check to see if it has 
+     * already passed on the current build.  If it has passed then we abort 
+     * test but make sure a GREEN bar is shown.  
      */
     private static final long serialVersionUID = -2530814572579866798L;
 
