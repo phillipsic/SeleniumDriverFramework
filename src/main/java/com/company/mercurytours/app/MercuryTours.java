3ed002d0eaa122f3bf9c276a709e1858379b9ae0
Ian Phillips <phillipsic@gmail.com>
2015-05-11 17:00:04 +0200

    Changed name of base URL in property file

--- src/main/java/com/company/mercurytours/app/MercuryTours.java
+++ src/main/java/com/company/mercurytours/app/MercuryTours.java
@@ -5,15 +5,15 @@ import com.framework.common.Application;
 public class MercuryTours extends Application {
 
     public MercuryTours() throws Exception {
-        setName("MercuryTours"); // Also used as a key in INI file for application name.
+        setName("MercuryTours");
         setCommonName("Mercury Tours");
-       
-        
+        setMainURLKey("MercuryTours_base_url");
+
         /*
-        The base URL should not be set here but in the <ENV>_init.properties file.
-        The base URL could change from one environment to another.
-        If there is no application name in the URL then leave out of the 
-        <ENV>_init.properties file.
-        */
+         The base URL should not be set here but in the <ENV>_init.properties file.
+         The base URL could change from one environment to another.
+         If there is no application name in the URL then leave out of the 
+         <ENV>_init.properties file.
+         */
     }
 }
