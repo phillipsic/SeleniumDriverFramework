ef23aa0dc62a976d0aa16736a332fa8fd6b43374
Ian Phillips <phillipsic@gmail.com>
2015-05-12 13:26:35 +0200

    Added more tests to test suite

--- src/test/java/com/company/egTestSuite/MercuryTours_TS.java
+++ src/test/java/com/company/egTestSuite/MercuryTours_TS.java
@@ -1,11 +1,16 @@
 package com.company.egTestSuite;
 
 import com.company.mercurytours.CHK001_RegisterUser;
+import com.company.mercurytours.CHK002_LaunchSecondApplication;
+import com.company.mercurytours.CHK003_RegisterUserBugIdExample;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ CHK001_RegisterUser.class})
+@SuiteClasses({CHK001_RegisterUser.class,
+    CHK002_LaunchSecondApplication.class,
+    CHK003_RegisterUserBugIdExample.class
+})
 public class MercuryTours_TS {
 }
