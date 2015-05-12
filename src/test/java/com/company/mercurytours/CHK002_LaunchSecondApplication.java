8ffc18f72291d054651a531f86f3ebea24c2ce86
Ian Phillips <phillipsic@gmail.com>
2015-05-12 11:03:15 +0200

    Added example for launching a second application in a test.

---
+++ src/test/java/com/company/mercurytours/CHK002_LaunchSecondApplication.java
@@ -0,0 +1,70 @@
+package com.company.mercurytours;
+
+import com.company.mercurytours.app.MercuryTours;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.framework.common.AlreadyRunException;
+import com.framework.app.common.AppTest;
+import com.framework.app.common.Common;
+import com.framework.app.common.Prep;
+import com.company.mercurytours.users.JoeBloggs;
+
+import com.company.mercurytours.pages.*;
+
+public class CHK002_LaunchSecondApplication extends AppTest {
+
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        preparation = new Prep();
+        application = new MercuryTours();
+        user = new JoeBloggs();
+    }
+
+    @Test
+    public void testCHK001_RegisterUser() throws Exception {
+
+        try {
+
+            launchApplication();
+
+            WelcomeMercuryTours WelcomePage = new WelcomeMercuryTours(tool, test, user);
+            RegisterMercuryTours RegisterPage = WelcomePage.clickRegisterLink();
+
+            RegisterPage.enterFirstName(user.getFirstName());
+            RegisterPage.enterLastName(user.getLastName());
+
+            RegisterPage.enterUserName(user.getLogin());
+            RegisterPage.enterPassword(user.getPassword());
+            RegisterPage.enterConfirmPassword(user.getPassword());
+            RegisterPage.clickSubmit();
+
+            Common.assertTextOnPage(tool, "Dear " + user.getFirstName() + " " + user.getLastName());
+            Common.assertTextOnPage(tool, "Note: Your user name is " + user.getLogin());
+
+          
+            launchAnotherApplication();
+            Common.assertCorrectPageTitle(tool, "Google");
+            
+            
+            test.setResult("pass");
+
+        } catch (AlreadyRunException e) {
+        } catch (Exception e) {
+            verificationErrors.append(e.getMessage());
+            throw e;
+        }
+
+    }
+
+    @Override
+    @After
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
