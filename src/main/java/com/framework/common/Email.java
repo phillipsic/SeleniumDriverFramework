0a8a449c53a26387d2a26ba1e21a7341fef0e45e
Ian Phillips <phillipsic@gmail.com>
2015-05-28 16:41:03 +0200

    Started to implement email checking #21

---
+++ src/main/java/com/framework/common/Email.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.framework.common;
+
+import java.util.Properties;
+
+/**
+ *
+ * @author S823509
+ */
+public class Email {
+
+    private int numberOfEmails;
+    private String emailSubject;
+    private String fromUser;
+    private String emailContent;
+
+    public void retrieveEmailMessageBySubject(String emailSubject) {
+
+        Properties properties = new Properties();
+
+        properties.put("mail.pop3.host", host);
+        properties.put("mail.pop3.port", "995");
+        properties.put("mail.pop3.starttls.enable", "true");
+        Session emailSession = Session.getDefaultInstance(properties);
+
+        //create the POP3 store object and connect with the pop server
+        Store store = emailSession.getStore("pop3s");
+
+        store.connect(host, user, password);
+
+        //create the folder object and open it
+        Folder emailFolder = store.getFolder("INBOX");
+        emailFolder.open(Folder.READ_ONLY);
+
+        // retrieve the messages from the folder in an array and print it
+        Message[] messages = emailFolder.getMessages();
+        System.out.println("messages.length---" + messages.length);
+
+        for (int i = 0, n = messages.length; i < n; i++) {
+            Message message = messages[i];
+            System.out.println("---------------------------------");
+            System.out.println("Email Number " + (i + 1));
+            System.out.println("Subject: " + message.getSubject());
+            System.out.println("From: " + message.getFrom()[0]);
+            System.out.println("Text: " + message.getContent().toString());
+
+        }
+
+        //close the store and folder objects
+        emailFolder.close(false);
+        store.close();
+    }
+
+    public void getNumberOfEmails() {
+
+    }
+}
